a21720585e62a24e9b94f9c892698d54492e1866
Satit Rianpit <siteslave@msn.com>
2015-03-23 14:09:23 +0700

    add redirect

--- src/login/LoginController.js
+++ src/login/LoginController.js
@@ -4,7 +4,12 @@ App.controller('LoginController', function ($scope, $window, LoginService, LxNot
         LoginService.login($scope.username, $scope.password)
             .then(function (rows) {
                 if (_.size(rows)) {
-                    LxNotificationService.success('ยินดีต้อนรับ ' + rows[0].fullname);
+                    // set session
+                    $window.sessionStorage.setItem('username', $scope.username);
+                    $window.sessionStorage.setItem('fullname', rows[0].fullname);
+
+                    $window.location.href = '../pages/Main.html';
+
                 } else {
                     LxNotificationService.error('ชื่อผู้ใช้งาน หรือ รหัสผ่านไม่ถูกต้อง');
                 }
