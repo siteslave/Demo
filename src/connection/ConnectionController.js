1d63b1fec2592213445250840774439283e037d4
Satit Rianpit <siteslave@msn.com>
2015-03-23 12:44:48 +0700

    change config

--- src/connection/ConnectionController.js
+++ src/connection/ConnectionController.js
@@ -1,8 +1,12 @@
 /**
  * Connection Controller
  */
-App.controller('ConnectionController', function ($scope) {
+App.controller('ConnectionController', function ($scope, LxNotificationService) {
 
     $scope.config = jf.readFileSync(configFile);
 
+    $scope.save = function () {
+        jf.writeFileSync(configFile, $scope.config);
+        LxNotificationService.success('บันทึกเสร็จแล้ว');
+    };
 });
