0927b8d106d13bc6fc335ff53ad8b0d87a5878bc
Satit Rianpit <siteslave@msn.com>
2015-03-24 14:52:16 +0700

    send data success

--- src/main/MainController.js
+++ src/main/MainController.js
@@ -1,7 +1,8 @@
 /**
  * Main Controller
  **/
-App.controller('MainController', function ($scope, MainService, LxDialogService, LxNotificationService) {
+App.controller('MainController', function ($scope, MainService, LxDialogService,
+                                           LxNotificationService, $window) {
     $scope.patient = [];
 
     $scope.getData = function (startDate, endDate) {
@@ -80,6 +81,60 @@ App.controller('MainController', function ($scope, MainService, LxDialogService,
                 console.log(err);
                 LxNotificationService.error('Error');
             });
+
+    };
+
+    $scope.doExport = function () {
+        //console.log($scope.patient);
+        var exportedPath = path.join(appPath, 'exported');
+        var exportedFile = path.join(exportedPath, 'accident.txt');
+        var header = 'hospcode|vstdate|vsttime|hn|vn|cid|pname|fname|lname|birth|sex|bba|dba|psychic\n';
+        fs.writeFileSync(exportedFile, header, {flag: 'w'});
+
+        var tmpData = [];
+        var dataToSend = [];
+
+        _.forEach($scope.patient, function (v) {
+            if (!v.exported_date) {
+                var obj = {};
+                obj.hospcode = $window.sessionStorage.getItem('hospcode');
+                obj.vstdate = moment(v.vstdate).format('YYYYMMDD');
+                obj.vsttime = v.vsttime;
+                obj.hn = v.hn;
+                obj.vn = v.vn;
+                obj.cid = v.cid;
+                obj.pname = v.pname;
+                obj.fname = v.fname;
+                obj.lname = v.lname;
+                obj.birth = moment(v.birthday).format('YYYYMMDD');
+                obj.sex = v.sex;
+                obj.bba = v.bba;
+                obj.dba = v.dba;
+                obj.psychic = v.psychic;
+                obj.updated_at = moment().format('YYYY-MM-DD HH:mm:ss');
+
+                dataToSend.push(obj);
+                tmpData.push({vn: obj.vn, exported_date: moment().format('YYYY-MM-DD HH:mm:ss')});
+
+                var data = [obj.hospcode, obj.vstdate, obj.vsttime, obj.hn,
+                    obj.vn, obj.cid, obj.pname, obj.fname, obj.lname, obj.birth,
+                    obj.sex, obj.bba, obj.dba, obj.psychic
+                ].join('|');
+
+                fs.appendFileSync(exportedFile, data + '\n');
+            }
+        });
+
+        var promise = MainService.sendRecord(dataToSend);
+        promise.then(function () {
+            MainService.saveLog(tmpData);
+        }).then(function () {
+            LxNotificationService.success('Success');
+            $scope.doGetData();
+        }, function (err) {
+            LxNotificationService.error('Error');
+            console.log(err);
+        });
 
     };
 
--- src/main/MainService.js
+++ src/main/MainService.js
@@ -23,9 +23,10 @@ App.factory('MainService', function ($q, Common) {
             db('er_nursing_detail as ed')
                 .select('o.vstdate', 'o.vsttime', 'o.hn', 'o.vn', 'p.cid',
                     'p.pname', 'p.fname', 'p.lname', 'p.birthday', 'p.sex',
-                    'ed.bba', 'ed.dba', 'ed.psychic')
+                    'ed.bba', 'ed.dba', 'ed.psychic', 'l.exported_date')
                 .innerJoin('ovst as o', 'o.vn', 'ed.vn')
                 .innerJoin('patient as p', 'o.hn', 'p.hn')
+                .leftJoin('nemo_send_logs as l', 'l.vn', 'ed.vn')
                 .whereBetween('o.vstdate', [startDate, endDate])
                 .orderBy('o.vstdate')
                 .exec(function (err, rows) {
@@ -48,6 +49,80 @@ App.factory('MainService', function ($q, Common) {
                     if (err) q.reject(err);
                     else q.resolve();
                });
+
+            return q.promise;
+        },
+
+        saveLog: function (data) {
+            var q = $q.defer();
+
+            db('nemo_send_logs')
+                .insert(data)
+                .exec(function (err) {
+                    if (err) q.reject(err);
+                    else q.resolve();
+                });
+
+            return q.promise;
+        },
+
+        sendRecord: function (data) {
+            var knex = require('knex')({
+                client: 'mysql',
+                connection: {
+                    host: 'localhost',
+                    port: 3306,
+                    database: 'nemo',
+                    user: 'sa',
+                    password: 'sa'
+                },
+                pool: {
+                    min: 0,
+                    max: 100
+                },
+                debug: true
+            });
+
+            var q = $q.defer();
+
+            knex('accident')
+                .insert(data)
+                .exec(function (err) {
+                    if (err) q.reject(err);
+                    else q.resolve();
+                });
+
+            return q.promise;
+        },
+
+        sendData: function (file) {
+            var q = $q.defer();
+            var knex = require('knex')({
+                client: 'mysql',
+                connection: {
+                    host: 'localhost',
+                    port: 3306,
+                    database: 'nemo',
+                    user: 'sa',
+                    password: 'sa'
+                },
+                pool: {
+                    min: 0,
+                    max: 100
+                },
+                debug: true
+            });
+
+            var currentDateTime = moment().format('YYYY-MM-DD HH:mm:ss');
+
+            knex.raw(
+                "LOAD DATA LOCAL INFILE ? REPLACE INTO TABLE accident FIELDS TERMINATED BY '|' LINES TERMINATED BY '\n' IGNORE 1 ROWS (hospcode, @vstdate, @vsttime, hn,vn, cid, pname, fname, lname, @birth, sex, bba, dba, psychic, @updated_at) SET vstdate=STR_TO_DATE(@vstdate, '%Y%m%d'), vsttime=STR_TO_DATE(@vsttime, '%H%i%s'), birth=STR_TO_DATE(@birth, '%Y%m%d'), updated_at=?;", [file, currentDateTime])
+                .then(function() {
+                    q.resolve();
+                }, function (err) {
+                    console.log(err);
+                    q.reject(err);
+                });
 
             return q.promise;
         }
